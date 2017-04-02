b17b20b8a55dce7366cda372e79521bce8987a5c
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-02 02:54:39 -0400

    Implemented the create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,19 @@ class PortfoliosController < ApplicationController
 	def index 
 		@portfolio = Portfolio.all 
 	end 
+    # New action 
+	def new 
+		@portfolio = Portfolio.new 
+	end 
+    # Create action 
+	def create 
+		@portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subTitle, :body))
+		respond_to do |format|
+		if @portfolio.save
+		   format.html { redirect_to portfolios_path, notice: "Your Created a portfolio"}
+		else 
+		   format.html { render :new}  
+		end	
+	end 
 end
+end 
