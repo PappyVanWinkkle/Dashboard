9a748264378c01cf7740837c6b49cd7ef9c2bed1
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-02 12:10:28 -0400

    Created some edit and update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,5 +17,22 @@ class PortfoliosController < ApplicationController
 		   format.html { render :new}  
 		end	
 	end 
-end
-end 
+   end 
+
+  def edit 
+  	  @portfolio = Portfolio.find(params[:id])
+  end 
+
+  def update 
+  	  @portfolio = Portfolio.find(params[:id])
+  	  respond_to do |format|
+  	  if @portfolio.update(params.require(:portfolio).permit(:title, :subTitle, :body))
+  	     format.html { redirect_to portfolios_path, notice: "Just updated the portfolio"}	
+  	  else 
+  	     format.html {render :edit}  
+  	  end  
+  end 
+end  
+
+
+end    
