6b31df62dad9aa9a0586b792629f5cc454ce4a21
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-02 16:14:56 -0400

    implemented the delete action of the app

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,5 +38,14 @@ def show
 	@portfolio = Portfolio.find(params[:id])
 end  
 
+def destroy 
+	@portfolio = Portfolio.find(params[:id])
+	@portfolio.destroy
+	respond_to do |format|
+	format.html { redirect_to portfolios_url, notice: "Just deleted your portfolio"}
+	end 
+end	
+
+
 
 end    
