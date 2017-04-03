b10ff3bbce7a62e7fd00a0295add3b757a233c70
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-02 21:23:17 -0400

    configured the routes.rb page of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
+
 
   get 'pages/about'
 
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'pages#home'
 end
