0b82ee1fde8995cc83fa92dab84e314a5abe3a1f
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-07 10:28:31 -0400

    implemented concerns in our app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406134910) do
+ActiveRecord::Schema.define(version: 20170407140130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170406134910) do
     t.integer  "percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
