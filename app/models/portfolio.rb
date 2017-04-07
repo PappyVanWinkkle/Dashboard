804a185e745a5d4f8996a62e8d36174ce0cbc8c0
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-07 00:58:58 -0400

    Implemented defaults for images of portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
 	def self.angular
     	where(subTitle: "Ruby on Rails development")
     end 
+    after_initialize :set_defaults
+
+    def set_defaults 
+       self.mainImage ||= "http://placehold.it/600x400"
+       self.thumb_image ||= "http://placehold.it/350x200" 
+    end 
 end
+
