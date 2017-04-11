2ce853406cc644a123bb28f36b0760e1da66451a
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-11 00:23:02 -0400

    Added some concerns for devise

--- db/migrate/20170410054642_devise_create_users.rb
+++ db/migrate/20170410054642_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       ## Custom attributes
-      t.string :name
+      t.string :name 
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20170410054642) do
     t.text     "body"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "status",     default: 0
     t.string   "slug"
+    t.integer  "status",     default: 0
     t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
     t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170410054642) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
