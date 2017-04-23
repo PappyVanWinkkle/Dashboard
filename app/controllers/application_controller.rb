190828a148de4805d5ab3c21762b658ef10649cf
paulchabz <Owner1@MacBookMacBook3.lan>
2017-04-23 01:16:43 -0400

    added the petergate gem to our app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   include DefaultPageContent
   include ConfigurePermittedParameters
   include SetSource
+  before_action :set_copyright
 
   def current_user
   	super || OpenStruct.new(name: "Guest User", first_name:
                                 "Guest", last_name: "User")
   end
-end 
+end
+
+def set_copyright
+  @copyright = CopyViewTool::Rendering.copyright 'Mark Williams', 'All rights Reserved'
+end
+
+module CopyViewTool
+  class Rendering
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
